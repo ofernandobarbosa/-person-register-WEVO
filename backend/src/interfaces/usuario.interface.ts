6fdc89f4e0f38d61d6951f603e1da8c2f6ef1779
Fernando Barbosa <ofernandobarbosa@gmail.com>
2022-08-27 00:41:29 -0300

    implementando interfaces

---
+++ backend/src/interfaces/usuario.interface.ts
@@ -0,0 +1,10 @@
+export interface UserInterface{
+    _id: any | string;
+    nome: string;
+    cpf: string;
+    email: string;
+    telefone: string;
+    sexo: string;
+    dataNascimento: Date;
+
+}
